9eda29c30f44bf0f2453a4e7d2e1567a62aa6fd2
pocketful <iveta.jac@gmail.com>
2023-04-05 22:15:53 +0300

    fix: avoid setLoadMore trigger on quick scrolling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,51 +1,68 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import CardList from '../components/Cards/CardList'
 import Container from '../components/UI/Container/Container'
 import Loader from '../components/UI/Loader/Loader'
 import Error from '../components/UI/Error/Error'
 import { fetchData } from '../api/fetchData'
 import useEffectAfterMounted from '../hooks/useEffectAfterMounted'
+import debounceLeadingCancel from '../utilities/debounceLeadingCancel'
 
 const PER_PAGE = 20
 
 const HomePage = () => {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [loadNewPhotos, setLoadNewPhotos] = useState(false)
   const [page, setPage] = useState(1)
   const [hasNextPage, setHasNextPage] = useState(false)
+  const [loadMore, setLoadMore] = useState(false)
   const [error, setError] = useState('')
   const [favourites, setFavourites] = useState(JSON.parse(localStorage.getItem('favourites')) || [])
 
-  const getPhotos = async () => {
+  const fetchPhotos = async () => {
     setLoading(true)
     const result = await fetchData(`curated?page=${page}&per_page=${PER_PAGE}`)
     if (result.error) {
       setError(result.error)
     } else {
-      setPhotos((prevPhotos) => [...prevPhotos, ...result.photos])
-      setHasNextPage(result.next_page)
-      setLoadNewPhotos(false)
+      if (result.next_page) {
+        setHasNextPage(true)
+        setPhotos((prevPhotos) => [...prevPhotos, ...result.photos])
+      } else {
+        setPhotos(result.photos)
+      }
     }
     setLoading(false)
   }
 
-  const loadNewPhotosOnScrollEnd = () => {
-    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 3) {
-      setLoadNewPhotos(true)
+  useEffect(() => {
+    fetchPhotos()
+  }, [page])
+
+  const handleLoadMore = useMemo(
+    () =>
+      debounceLeadingCancel(() => {
+        setLoadMore(true)
+      }, 250),
+    [],
+  )
+
+  const handleScrollForLoadMore = () => {
+    const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.scrollHeight
+    if (scrolledToBottom) {
+      handleLoadMore()
     }
   }
 
   useEffect(() => {
-    getPhotos()
-    window.addEventListener('scroll', loadNewPhotosOnScrollEnd)
-    return () => window.removeEventListener('scroll', loadNewPhotosOnScrollEnd)
-  }, [page])
+    window.addEventListener('scroll', handleScrollForLoadMore)
+    return () => window.removeEventListener('scroll', handleScrollForLoadMore)
+  }, [])
 
   useEffectAfterMounted(() => {
-    if (!loadNewPhotos || !hasNextPage || loading) return
+    if (!loadMore || !hasNextPage || loading) return
     setPage((prevPage) => prevPage + 1)
-  }, [loadNewPhotos])
+    setLoadMore(false)
+  }, [loadMore])
 
   const toggleFavouriteHandler = (photoId) => {
     setFavourites((prevFavourites) => {
